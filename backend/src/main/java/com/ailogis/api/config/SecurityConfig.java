cf5fc92285112d044125ee5e3e6a30db3a77e4df
long230205 <long230205@gmail.com>
2026-06-30 07:06:40 +0700

    chore(security): config production cors for logicha.io.vn and secure ddl-auto

--- backend/src/main/java/com/ailogis/api/config/SecurityConfig.java
+++ backend/src/main/java/com/ailogis/api/config/SecurityConfig.java
@@ -64,10 +64,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        // Domain Frontend
-        // TEST ONLY: Chấp nhận mọi nguồn (chỉ dùng cho Dev/Test)
-        configuration.setAllowedOriginPatterns(List.of("*"));
-//        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://www.logicha.io.vn",
+                "https://logicha.io.vn"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
